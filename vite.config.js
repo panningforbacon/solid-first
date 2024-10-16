347661e8933b711eb021a937566207fa3178a631
Steve (Ipsos) <panningforbacon@gmail.com>
2024-10-15 19:23:24 -0500

    Adding SolidJS devtools

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
-    // devtools(),
+    devtools(),
     solidPlugin(),
   ],
   base: '/solid-first/',
